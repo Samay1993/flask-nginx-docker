379daf55ceb41de2cddd2f666346486baae68378
Samay Singh <samay.samay987@gmail.com>
2021-05-04 04:46:33 -0700

    Changing view to a descriptive one

--- flask/app/views.py
+++ flask/app/views.py
@@ -3,3 +3,6 @@ from app import app
 @app.route('/')
 def index():
   return "Hello from Flask in Develop Branch"
+  return "This is a production environment."
+  return "Deployed in Amazon EC2 Instance"
+  return "This is Jenkins Slave Node Fetching changes from Develop branch of git Repository, then Building and Deploying the application"
